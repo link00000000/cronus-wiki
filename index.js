cbfe06c5c5064262ede9b7354ab6db76041de811
link00000000 <crandall.logan@gmail.com>
2020-05-21 16:12:34 -0400

    Remove host variable

--- index.js
+++ index.js
@@ -3,13 +3,12 @@ const bodyParser = require('body-parser')
 
 const app = express()
 
-const HOST = process.env.HOST || '127.0.0.1'
 const PORT = process.env.PORT || 8080
 
 app.use(express.static('static'))
 app.use('/lib', express.static('lib'))
 app.use(bodyParser.json())
-app.listen(PORT, HOST, () => console.log(`Listening on at http://${HOST}:${PORT}`))
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
 
 app.use('/', require('./routers/docsifyStackEditPlugin'))
 app.use('/', require('./routers/docsifyDeletePlugin'))
